fff5dde877d787b37e1354370a0ce4657407878d
Marnie Sgouros <marnie.biando@gmail.com>
2019-04-03 20:10:51 -0600

    updated to fix error found

--- schemas/pat2ill_Schema.sql
+++ schemas/pat2ill_Schema.sql
@@ -1,7 +1,8 @@
 CREATE TABLE pat2ill (
 pat_id int,
+user_id int,
 ill_id int,
-ill_name text,
+ill_desc text,
 FOREIGN KEY (pat_id) REFERENCES patients(pat_id),
 FOREIGN KEY (ill_id) REFERENCES illnesses(ill_id)
 );
