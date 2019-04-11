38a0f859e6dd8cf3111a1cd762dadf2070bb7080
Marnie Sgouros <marnie.biando@gmail.com>
2019-04-11 07:09:41 -0600

    update creatData Diagnoses file

--- create_data/createData_Diagnoses.py
+++ create_data/createData_Diagnoses.py
@@ -7,22 +7,35 @@ Created on Sat Aug 11 07:23:18 2018
 NOSQL PROJECT 2 - SmallTown Hospital
 LOAD ICD9 Diagnoses CSV file, sample 1,000 then generate subset CSV
 
-enumerated output file at /Users/sgourosfamily/nosql-db/Project2/data
+enumerated output file at ./nosql-db/Project2/data
 """
 
 import pandas as pd
 import numpy as np
+import os
 
-filename = '/Users/sgourosfamily/GitHub_Repositories/NoSQL_Project2/data/ICD-9-Diagnoses.csv'
+# get data from sibling directory
+def getfile(sibling_dir, filename):
+    
+    parent_dir = os.path.split(os.getcwd())[0]
+    sibling_dir = os.path.join(parent_dir, sibling_dir)
+    datafile_path = os.path.join(sibling_dir, filename)
+    return datafile_path
+
+
+sibling_dir = 'data'
+ICD_9_file = 'ICD-9-Diagnoses.csv'
 nlines_in_file = 14568
 nlinesrandomsample = 10
 
 lines2skip = np.random.choice(np.arange(1,nlines_in_file+1), 
                               (nlines_in_file-nlinesrandomsample), 
                               replace=False)
-df = pd.read_csv(filename, skiprows=lines2skip)
 
-print(nlinesrandomsample,  "rows sampled from ICD9 DIAGNOSES file: ", filename)
+print("...now retrieving file: ", ICD_9_file)
+df = pd.read_csv(getfile(sibling_dir, ICD_9_file), skiprows=lines2skip)
+
+print(nlinesrandomsample,  "rows sampled from ICD9 DIAGNOSES file: ", ICD_9_file)
 print("")
 
 # add a numbered index
@@ -49,7 +62,7 @@ df.rename(columns={"ILL_ID": "id",
                               "DIAGNOSIS CODE":"icd9"}, inplace=True)
 
 # Write out udpated data to csv:
-df.to_csv('/Users/sgourosfamily/GitHub_Repositories/NoSQL_Project2/data/illnesses_test.csv',
+df.to_csv('../data/illnesses_test.csv',
                    index=False)
 print("Illness data loaded into CSV format....")
 
